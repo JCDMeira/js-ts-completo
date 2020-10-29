e02a2ddd7d39383dc295f931d5511c7f24a43cb0
Jean Meira <jean.meira10@hotmail.com>
2020-10-29 08:57:02 -0300

    deletando duplicatas

--- aula005_exConsole/index.js
+++
@@ -1,3 +0,0 @@
-//*Execício com console
-
-console.log('Meu nome é "Jean". Estou aprendendo JavaScript às', 10, 'da manhã')
--- aula018_alertConfirmPrompt/exercicio1/index.js
+++
@@ -1,9 +0,0 @@
-let num1 = prompt('digite um número');
-let num2 = prompt('digite outro número');
-
-num1 = Number(num1);
-num2 = Number(num2);
-
-const resultado = num1 + num2;
-
-alert(`O resultado da soma entre ${num1} e ${num2} é: ${resultado}`);
