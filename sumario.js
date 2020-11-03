080bbacdf1caab62294876b45c236509bcdb90ae
JCDMeira <jean.meira10@hotmail.com>
2020-11-03 13:54:30 -0300

    Adição de sumário de seções

---
+++ sumario.js
@@ -0,0 +1,34 @@
+/* 
+* SUMÁRIO
+    _Seção 1:Configurando o ambiente no Windows e Linux
+    _Seção 2:JavaScript Básico - Para iniciantes
+    _Seção 3:JavaScript - Lógica de programação
+    _Seção 4:JavaScript Funções (Avançado)
+    _Seção 5:JavaScript Arrays (Avançado)
+    _Seção 6:JavaScript Objetos e prototypes (Avançado)
+    _Seção 7:JavaScript Classes
+    _Seção 8:JavaScript assíncrono
+    _Seção 9:Javascript Tooling e ES6 Modules
+    _Seção 10:JavaScript Node (NodeJS)
+    _Seção 11:JavaScript + Node - Projeto Agenda (com Express + MongoDB)
+    _Seção 12:Linux - Deploy da aplicação
+    _Seção 13:API REST - JavaScript, Node, Express e SQL
+    _Seção 14:React (Básico) - JavaScript
+    _Seção 15:React + Redux + Redux Saga (Avançado) - JavaScript
+    _Seção 16:Expressões regulares em JavaScript - Regex (Bônus gratuito)
+    _Seção 17:TypeScript - Instalação
+    _Seção 18:TypeScript - Tipos básicos
+    _Seção 19:TypeScript - Classes e Interfaces (POO)
+    _Seção 20:TypeScript - Tipos avançados
+    _Seção 21:TypeScript - Generics (Generic Type)
+    _Seção 22:TypeScript - Decorators
+    _Seção 23:TypeScript - Namespaces e modules
+    _Seção 24:TypeScript - Princípios SOLID (S.O.L.I.D.)
+    _Seção 25:TypeScript - Testes com Jest
+    _Seção 26:Typescript e React
+    _Seção 27:TypeScript - Design Patterns (Padrões de projeto)
+    _Seção 28:NextJS + Strapi
+    _Seção 29:Deploy NextJS + Strapi no Ubuntu Server 20.04
+    _Seção 30:Notas finais
+    _Seção 31:## MAIS BÔNUS GRATUITOS ##
+*/
