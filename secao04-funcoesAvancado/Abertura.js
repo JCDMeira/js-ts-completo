9fad86a7affa8769b69746427730e27eaee25af5
JCDMeira <jean.meira10@hotmail.com>
2020-11-16 16:17:13 -0300

    Adição da aula 64

--- secao04-funcoesAvancado/Abertura.js
+++
@@ -1,5 +0,0 @@
-/*
-    #Nota#
-    _ Abertura de seção para organização do gitHub, logo será adicionado conteúdo
-    _ e excluido esse arquivo de nota.
-*/
--- secao04-funcoesAvancado/aula064_declararcao/index.js
+++ secao04-funcoesAvancado/aula064_declararcao/index.js
@@ -1,1 +1,46 @@
 //* Aula 64 - Maneiras de declarar funções
+
+//| declaração
+function falaOi(){
+    console.log('Oi');
+};
+//_ É uma declaração mais literal.
+//_ Ocorre o function hoisting -> eleva a declaração.
+//_ declaração mais clássica.
+
+falaOi();
+
+//| functions expressions
+//_ função (todas) são first-class objects (objetos de primeira classe)
+    //_ podem ser tratadas como dado.
+
+    const souUmDado = function(){
+        console.log('Sou um dado');
+    }
+
+    souUmDado();
+//_ Pode executar a constante ou variável como uma função.
+//_ Permite passar funções como parâmetros.
+
+function executaFuncao(funcao){
+    funcao();
+}
+
+executaFuncao(souUmDado);
+
+//| Arrow function -> function expression resumida na declaração
+
+const arrow = ()=> console.log('Sou uma arrow function');
+
+arrow();
+
+//| Dentro de objetos
+
+const obj = {
+    //falar: function(){    //? abaixo tem o modo mais novo de declarar um método do objeto.
+    falar(){
+        console.log('Estou falando...');
+    }
+}
+
+obj.falar();
