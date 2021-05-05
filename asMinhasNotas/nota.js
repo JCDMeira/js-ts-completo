e719d67d829f8363613942e171d304318473c8b7
JCDMeira <jean.meira10@hotmail.com>
2021-05-05 11:26:42 -0300

    Adiciona pasta para notas importantes

---
+++ asMinhasNotas/nota.js
@@ -0,0 +1,7 @@
+/*  
+    _ Alguns tipos de valores são imutáveis, como number, string, boolean,
+    _ undefined, null, symbol e bigint 
+    _ (os primitivos todos são imutáveis). Outros tipos são mutáveis, como arrays e objetos 
+    _ (objetos em geral são mutáveis, com exceção de null).
+
+*/
