24457a44324a751245ffe62f8a3392b04ff785e1
JCDMeira <jean.meira10@hotmail.com>
2021-05-05 11:42:14 -0300

    Adiciona nota explicativa de como usar melhor as notas

---
+++ asMinhasNotas/ComoUsar.js
@@ -0,0 +1,35 @@
+//* Explicando como usar de forma mais aprimorada as notas
+
+/*
+    ! As notas desse projetos usam o Better Comments para marcação colorida 
+    ! tornando assim as notas mais evidentes e um pouco mais semânticas.
+    @ no README.md do projeto está disponível as configurações que uso para
+    @ a extensão. Mas são as tags:
+    !importante
+    @aviso
+    #obs
+    _nota
+    *Título
+    
+    # Há as formas diminutivas, que por si só já funcionam, mas achei melhor 
+    # criar a redundância com as tags por extenso por se tratar de criar a 
+    # facilidade para quem começar a adotar.
+    @ as tags simplificadas são
+    !   qualquer nota importante
+    @   qualquer aviso
+    #   qualquer observação
+    _   qualquer nota
+    *   qualquer título
+
+
+    # Há ainda tags que não são diminutivas, como:
+    ?  -> a interrogação normalmente usada para notas rápidas, ou principalmente códigos.
+    |  -> usado como marcação ou sub-títulos
+    -  -> usado como um sub-nível das notas
+    //
+    # usa // dentro de um comentário longo ou em adição as duas barras padrões de comentário
+    # curto no geral indica algo que já foi acabado ou que foi removido, porém normalmente 
+    # só uso com notas colaborativos ou cronogramadas.
+
+    ! enjoy those notes :)
+*/
