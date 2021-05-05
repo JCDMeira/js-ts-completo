1d53d493b945892cacf81824497fe0d82d4e9915
JCDMeira <jean.meira10@hotmail.com>
2021-05-05 11:27:56 -0300

    Adiciona notas centralizadas do projeto da seção HTML/CSS (links importantes)

---
+++ asMinhasNotas/sobreHTML.js
@@ -0,0 +1,8 @@
+/*
+    !importante! https://validator.w3.org/nu/#textarea (validador de código HTML)
+    !importante! https://achecker.ca/checker/index.php (Accessinility check)
+        #obs# Background SVG 
+        - https://www.svgbackgrounds.com/#confetti-doodles
+        #obs# unDraw 
+        -https://undraw.co
+*/
