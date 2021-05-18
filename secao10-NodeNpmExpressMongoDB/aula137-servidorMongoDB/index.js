0c8ca714383bc596cb4a5d7faddecd99831ebdc6
JCDMeira <jean.meira10@hotmail.com>
2021-05-18 15:16:09 -0300

    Adiciona aula138 - sobre mongoDB

---
+++ secao10-NodeNpmExpressMongoDB/aula137-servidorMongoDB/index.js
@@ -0,0 +1,9 @@
+//* Aula 137 - Criando um servidor MongoDB
+/*
+    _Nota_
+    _ https://www.mongodb.com/cloud/atlas
+
+    ! mongodb+srv://jeanmeira:<password>@cluster0.8zh4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+    _ Link de conexão do mongo db
+*/
