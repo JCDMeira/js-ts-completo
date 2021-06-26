ac9010cbc248efc5bcd6002e9d8ef36cbc969a6e
JCDMeira <jean.meira10@hotmail.com>
2021-06-25 22:28:27 -0300

    Atualiza aula 245 - sobre lookaround

--- secao16-expressoesRegulares/aula245-lookaheadLookbehind/index.js
+++ secao16-expressoesRegulares/aula245-lookaheadLookbehind/index.js
@@ -6,7 +6,37 @@
     _Nota_
     _ 
 */
-const { html2 } = require("../base");
+const { lookahead } = require("../base");
 
-// console.log(html2.match(/<(\w+)[\s\S]*?>([\s\S]*?)<\/\1>/g));
-console.log(html2.replace(/(<(\w+)(?:[\s\S]*?)>)([\s\S]*?)(<\/\2>)/g, "$1 HAHA$3HAHA $4"));
+// console.log(lookahead);
+// console.log(lookahead.match(/.+[^in]active$/gim));
+
+//_ positive lookahead (frases que tem active)
+// console.log(lookahead.match(/.+(?=[^in]active)/gim));
+
+//_ positive lookahead (frases que tem inactive)
+// console.log(lookahead.match(/.+(?=\s+inactive)/gim));
+
+//_ negative lookahead (frases que não tem inactive)
+// console.log(lookahead.match(/^(?!.+[^in]active).+$/gim));
+
+//_ negative lookahead (frases que não tem inactive)
+// console.log(lookahead.match(/^(?!.+inactive).+$/gim));
+
+//_ positive lookbehind (frases começam com online)
+// console.log(lookahead.match(/(?<=ONLINE\s+)\S+.*/gim));
+
+//_ negative lookbehind (frases não começam com online)
+console.log(lookahead.match(/^.+(?<!online.+)$/gim));
+
+const cpf = `
+012.250.796-10
+000.000.000-01
+111.111.111-11
+999.999.999-99
+555.555.555-55
+147.285.963-10
+aaa.bbb.ccc-dd
+`
+
+console.log(cpf.match(/^(?!^(\d)\1{2}\.\1{3}\.\1{3}-\1{2}$)\d{3}\.\d{3}\.\d{3}\-\d{2}$/gm));
