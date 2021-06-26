cf1b32ff37a780235bcf69c9e16d6219ffef9219
JCDMeira <jean.meira10@hotmail.com>
2021-06-25 22:49:43 -0300

    Atualiza expressões regulares úteis as minhas notas

--- asMinhasNotas/expressoesRegulares.js
+++ asMinhasNotas/expressoesRegulares.js
@@ -30,15 +30,18 @@ const validaSenhasFortes = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\]\)]).
 // # Validar e-mails (use a última) #
 // ##################################
 
-// [^\s]+@[^\s]+\.[^\s]+(\w+)* (Hiper Permissiva)
+// - (Hiper Permissiva)
+// - tem acertos, mas ainda pega inválidos
+//// [^\s]+@[^\s]+\.[^\s]+(\w+)* 
 
-// Essa expressão é bem permissiva, menos que a anterior, 
-// Mas ainda detecta bastante e-mails inválidos
-// [^\s\.]+\.?[^\s\.]+@[^\s]+\.[^\s]+\w+
+//- Essa expressão é bem permissiva, menos que a anterior, 
+//- Mas ainda detecta bastante e-mails inválidos
+//// [^\s\.]+\.?[^\s\.]+@[^\s]+\.[^\s]+\w+
 
-// Em meus testes a regexp que obteve melhor resultado 
-// (acertando mais válidos e inválidos) 
-// Foi a da linha a seguir:
-// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//- Em meus testes a regexp que obteve melhor resultado 
+//- (acertando mais válidos e inválidos) 
+//- Foi a da linha a seguir:
+//// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+// @ Usar essa expressão para a validação
 const validaEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
