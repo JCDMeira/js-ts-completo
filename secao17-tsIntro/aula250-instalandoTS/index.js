45f985dc5ee912a2f790f83fa10a7d74e5cd7b65
JCDMeira <jean.meira10@hotmail.com>
2021-06-29 23:13:07 -0300

    Adiciona aula 250 - instalando typescript

---
+++ secao17-tsIntro/aula250-instalandoTS/index.js
@@ -0,0 +1,5 @@
+// * Testando ts
+function greet(name) {
+    return "Ol\u00E1 " + name;
+}
+console.log(greet("Jean"));
---
+++ secao17-tsIntro/aula250-instalandoTS/index.ts
@@ -0,0 +1,7 @@
+// * Testando ts
+
+// ! comando para compilar em .js --> npx tsc index.ts
+
+function greet(name : string ) {
+    return `Olá ${name}`
+}
