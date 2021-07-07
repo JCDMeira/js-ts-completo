76f5d0fa4d883dc9989b570b5a3e8f6cff778dbd
JCDMeira <jean.meira10@hotmail.com>
2021-07-06 22:28:10 -0300

    Adiciona aula 279 - classes com TS

---
+++ secao19-TS_POO/src/aula279-classesTS/index.ts
@@ -0,0 +1,19 @@
+//  * Aula 278 - Os pilares da programação orientada a objetos (POO)
+
+/*
+    _Notas
+    _ Abstração
+      - Permite isolar de um objeto somente os conceitos que são necessários
+      - para o funcionamento do programa.
+    _ Encapsulamento
+      - Visa ocultar partes internas de um objeto e exibir apenas o necessário
+      - para uso externo.
+      # É basicamente ocultar aquilo que você não quer que fique esposto.
+    _ Herança
+      - Visa passar a característica de um objeto para outro.
+      # Faz com que os objetos filhos tenham características herdadas dos pais.
+    _ Polimorfismo
+      - Algo que é polimorfo tem a habilidade de assumir diferentes formas.
+      # Permite mudar as características de algo herdado do pai, fazendo assumir
+      # mais de um comportamento
+*/
