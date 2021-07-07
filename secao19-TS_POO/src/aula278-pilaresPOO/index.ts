80fc5381f837c76d76e63b05ffacbe97dba14dc1
JCDMeira <jean.meira10@hotmail.com>
2021-07-06 22:29:42 -0300

    Corrige numeração de pastas 278 e 279

--- secao19-TS_POO/src/aula278-pilaresPOO/index.ts
+++ secao19-TS_POO/src/aula278-pilaresPOO/index.ts
@@ -1,64 +1,19 @@
-//  * Aula 279 - Classes em TypeScript - Moldes para objetos
+//  * Aula 278 - Os pilares da programação orientada a objetos (POO)
 
 /*
     _Notas
-    _ classes também são tipos
-    _ o public é redundante, caso não tenha nada indicado o
-    _ atributo será public
-
-    _ O structural typing faz com que ao receber um objeto
-    _ que contenha as mesmas chaves que a regra definida no
-    _ constructor seja uma opção válida.
+    _ Abstração
+      - Permite isolar de um objeto somente os conceitos que são necessários
+      - para o funcionamento do programa.
+    _ Encapsulamento
+      - Visa ocultar partes internas de um objeto e exibir apenas o necessário
+      - para uso externo.
+      # É basicamente ocultar aquilo que você não quer que fique esposto.
+    _ Herança
+      - Visa passar a característica de um objeto para outro.
+      # Faz com que os objetos filhos tenham características herdadas dos pais.
+    _ Polimorfismo
+      - Algo que é polimorfo tem a habilidade de assumir diferentes formas.
+      # Permite mudar as características de algo herdado do pai, fazendo assumir
+      # mais de um comportamento
 */
-
-export class Empresa {
-  public readonly nome : string;
-  //  _ Faz apenas o atributo ser imutável
-  private readonly colaboradores: Colaborador[] = [];
-  //  _ Esse modelo faria o array ser imutável.
-  // private  colaboradores: readonly Colaborador[] = [];
-  protected readonly cnpj: string
-
-  constructor(nome: string, cnpj: string){
-      this.nome = nome;
-      this.cnpj = cnpj;
-  }
-
-  public adicionaColaborador(colaborador: Colaborador): void {
-      this.colaboradores.push(colaborador);
-  }
-
-  public mostrarColaboradores(): void {
-      for(const colaborador of this.colaboradores) {
-          console.log(colaborador);
-
-      }
-  }
-}
-
-export class Colaborador{
-    constructor(
-      public readonly nome: string,
-      public readonly sobrenome: string
-    ){}
-}
-
-const empresa1 = new Empresa("Udemy", "11.111.111/0001-11");
-console.log(empresa1);
-// empresa1.nome = "novo nome"; //  # não permite alterar pelo readonly
-console.log(empresa1.nome);
-
-const colaborador1 = new Colaborador("Jean", "Meira");
-const colaborador2 = new Colaborador("Maria", "Miranda");
-const colaborador3 = new Colaborador("João", "Vieira");
-
-empresa1.adicionaColaborador(colaborador1);
-empresa1.adicionaColaborador(colaborador2);
-empresa1.adicionaColaborador(colaborador3);
-console.log(empresa1);
-console.log("----------------");
-empresa1.adicionaColaborador({
-    nome: "nome",
-    sobrenome: "Usando o structural typing"
-});
-empresa1.mostrarColaboradores();
