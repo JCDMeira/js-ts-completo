4c6aefb91f44f08c03317aa5754b37df9e0571c1
JCDMeira <jean.meira10@hotmail.com>
2021-07-07 22:11:49 -0300

    Adiciona aula 280 - revisão tsconfig

---
+++ secao19-TS_POO/src/aula280-tsconfig/indext.ts
@@ -0,0 +1,13 @@
+//  * Aula 280 - Revisando o tsconfig.json
+
+/*
+    _Nota
+    - Encadeamento opcional
+    - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+    _ Operador de coalescência nula
+    -https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+    _ String.prototype.replaceAll()
+    -https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+    _ compt-table
+    -https://kangax.github.io/compat-table/es2016plus/#node12_11
+*/
