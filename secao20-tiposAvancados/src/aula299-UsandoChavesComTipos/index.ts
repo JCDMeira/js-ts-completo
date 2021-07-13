6fed77ca2ed8e06478d80ea32571d4011224988c
JCDMeira <jean.meira10@hotmail.com>
2021-07-12 22:40:01 -0300

    Adiciona aula 300 - usando this como tipo

--- secao20-tiposAvancados/src/aula299-UsandoChavesComTipos/index.ts
+++ secao20-tiposAvancados/src/aula299-UsandoChavesComTipos/index.ts
@@ -1,26 +1,66 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-//  * Aula 299 - Usando chaves com tipos
+//  * Aula 300 - Usando this como tipo
 
 /*
     _Nota_
-    _ usa um espelhamento de classes, em que
-    _ uma chave de um objeto type é usado para
-    _ definir o valor de outro.
+    @ this polimorfico
 */
 
-type Veiculo = {
-  marca: string;
-  ano: string;
+export class Calculadora {
+    constructor(public numero: number) {  }
+
+    add(n: number): this {
+        this.numero += n;
+        return this;
+    }
+
+    sub(n: number): this {
+        this.numero -= n;
+        return this;
+    }
+
+    div(n: number): this {
+        this.numero /= n;
+        return this;
+    }
+
+    mul(n: number): this {
+        this.numero *= n;
+        return this;
+    }
 }
 
-type Car = {
-  brand: Veiculo["marca"];
-  year: Veiculo["ano"];
-  name: string;
+export class subCalculadora extends Calculadora {
+    pow(n: number): this {
+        this.numero **= n;
+        return this;
+    }
 }
 
-const carro: Car = {
-    brand: "Ford",
-    year: "2020",
-    name: "Nome"
-};
+const calculadora = new subCalculadora(10);
+
+calculadora.add(5).mul(2).div(2).sub(5).pow(2);
+console.log(calculadora);
+
+//  _ Builder - GoF
+export class RequestBuilder {
+  private method: "get" | "post" | null = null;
+  private url: string | null = null;
+
+  setMethod(method: "get" | "post"): this {
+      this.method = method;
+      return this;
+  }
+
+  setUrl(url: string): this {
+      this.url = url;
+      return this;
+  }
+
+  send(): void {
+      console.log(`Enviando dados via ${this.method} para ${this.url}`);
+  }
+}
+
+const request = new RequestBuilder();
+request.setUrl("http://www.google.com").setMethod("post").send();
