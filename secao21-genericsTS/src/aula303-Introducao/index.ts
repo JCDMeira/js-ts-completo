14c7595c26cb87a227f329d39eb1b8626cef49d7
JCDMeira <jean.meira10@hotmail.com>
2021-07-14 22:50:21 -0300

    Atualiza aula 303 - introdução aos tipos genéricos

--- secao21-genericsTS/src/aula303-Introducao/index.ts
+++ secao21-genericsTS/src/aula303-Introducao/index.ts
@@ -4,3 +4,24 @@
 /*
     _Nota_
 */
+
+type FilterCallback <U> =  (value: U, index?: number, array?: U[])=> boolean;
+
+export function meuFilter <T>(array: T[], callbackFn: FilterCallback<T> ): T[] {
+    const novoArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if(callbackFn(array[i])) {
+            novoArray.push(array[i]);
+        }
+    }
+
+    return novoArray;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayFiltradoOriginal = array.filter((value => value < 5));
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value => value < 5));
+console.log(arrayFiltrado);
