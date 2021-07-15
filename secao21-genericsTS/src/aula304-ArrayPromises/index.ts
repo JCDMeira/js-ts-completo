c8a2d813cc841fa760d333d6cd2c85ace6699b9a
JCDMeira <jean.meira10@hotmail.com>
2021-07-14 23:13:14 -0300

    Corrige inversão de aulas 304 e 305

--- secao21-genericsTS/src/aula304-ArrayPromises/index.ts
+++ secao21-genericsTS/src/aula304-ArrayPromises/index.ts
@@ -1,45 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-//  * Aula 305 - Generics com interfaces e Type Alias
+//  * Aula 304 - Array e Promises são generics
 
 /*
     _Nota_
 */
 
-interface PessoaProtocolo <T = string, U= number> {
-  nome: T;
-  sobrenome: T;
-  idade: U
+type MeuTipo = number;
+
+const arrayNumeros: Array<number> = [ 1, 2, 3, 4, 5, 6];
+
+async function promiseAsync() {
+    return 1;
 }
 
-type PessoaProtocolo2 <T = string, U= number> = {
-  nome: T;
-  sobrenome: T;
-  idade: U
+function minhaPromise(): Promise<MeuTipo> {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve(1);
+        }, 1000);
+    });
 }
 
-const Aluno1: PessoaProtocolo = {
-    nome: "Luiz",
-    sobrenome: "miranda",
-    idade: 30
-};
-
-const Aluno2: PessoaProtocolo<number, number> = {
-    nome: 123,
-    sobrenome: 456,
-    idade: 30
-};
-
-const Aluno3: PessoaProtocolo2 = {
-    nome: "Luiz",
-    sobrenome: "miranda",
-    idade: 30
-};
-
-const Aluno4: PessoaProtocolo2<number, number> = {
-    nome: 123,
-    sobrenome: 456,
-    idade: 30
-};
-
-console.log(Aluno1, Aluno2);
-console.log(Aluno3, Aluno4);
+promiseAsync().then((resultado) => console.log(resultado + 1));
+minhaPromise().then((resultado) => console.log(resultado + 1));
