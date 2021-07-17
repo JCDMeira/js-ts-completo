dc73329fc6ddb515b577b048fb95a2337a42d4e5
JCDMeira <jean.meira10@hotmail.com>
2021-07-16 22:47:20 -0300

    Adiciona aula 313 - introdução aos decorators

--- secao22-decorators/src/aula313-Introducao/index.ts
+++ secao22-decorators/src/aula313-Introducao/index.ts
@@ -11,24 +11,24 @@
 
 @decorator
 export class Animal {
-  constructor(public nome: string, public cor: string) {}
+    constructor(public nome: string, public cor: string) {}
 }
 
 function decorator<T extends new (...args: any[])=> any>(target: T): T {
-  return class extends target {
+    return class extends target {
     cor: string;
     nome: string;
 
     constructor(...args: any[]) {
-      super(...args);
-      this.nome = this.inverte(args[0])
-      this.cor = this.inverte(args[1])
+        super(...args);
+        this.nome = this.inverte(args[0]);
+        this.cor = this.inverte(args[1]);
     }
 
     inverte(valor: string): string {
-      return valor.split("").reverse().join("");
+        return valor.split("").reverse().join("");
     }
-  };
+    };
 }
 
 // const AnimalDecorated = decorator(Animal)
