fe76722db212f4fe53cdbc0d44c984ba04540d6c
JCDMeira <jean.meira10@hotmail.com>
2021-07-22 20:32:32 -0300

    Adiciona aula 324 - importando js em ts

---
+++ secao23-namespaceModules/src/aula324-importandoJsEmTs/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 324 - Importando JavaScript no TypeScript
+
+/*
+    _Nota_
+*/
+
+import {soma} from "./modulo";
+
+const result = soma(10, 20) as number;
+console.log(result);
