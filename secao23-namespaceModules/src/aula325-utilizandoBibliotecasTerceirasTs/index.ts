e16698747cd21ae656c24f706ec8a7a84f4c2a7f
JCDMeira <jean.meira10@hotmail.com>
2021-07-22 20:40:07 -0300

    Adiciona aula 325 - utilizando bibliotecas de terceiros

---
+++ secao23-namespaceModules/src/aula325-utilizandoBibliotecasTerceirasTs/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 325 - Utilizando bibliotecas de terceiros no TS
+
+/*
+    _Nota_
+    _ instalar os tipos
+    ! @types/(pacote)
+*/
