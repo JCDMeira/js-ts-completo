ccd8b1cb616a7031de8125530665f804ce51a889
JCDMeira <jean.meira10@hotmail.com>
2021-07-23 14:32:14 -0300

    Adiciona aula 327 - introdução ao SOLID

---
+++ secao24-SOLID/src/aula327-introducao/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 327 - O que são os princípios de SOLID
+
+/*
+    _Nota_
+    @ https://en.wikipedia.org/wiki/SOLID
+    _ manter código limpo, flexível e fácil de ser mantido.
+    _ S - single responsability principle (princípio da responsabilidade única)
+    _ O - Open/Cloosed principle (princípio do aberto/fechado)
+    _ L - Liskov substituition principal (Princípio da substituição de liskov)
+    _ I - Interface segregation principla (princípio da agregação de interface)
+    _ D - Dependency inversion principle ( princípio da inversão de dependência)
+*/
