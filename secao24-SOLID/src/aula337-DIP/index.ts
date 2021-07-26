c37161cb0909ca20670fd62547b122177c1070db
JCDMeira <jean.meira10@hotmail.com>
2021-07-25 23:48:46 -0300

    Adiciona aula 337 - DIP

---
+++ secao24-SOLID/src/aula337-DIP/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 337 - DIP - Dependency inversion principle
+//  * (princípio da inversão de dependência)
+
+/*
+    _Nota_
+    _ Móduloes de alto nível não devem depender de módulos de baixo nível.
+    _ Ambos devem depender de abstrações.
+    _ Dependa de abstrações, não de implementações.
+    _ Abstrações não deve, depender de detalhes. Detalhes devem depender de
+    _ abstrações.
+
+    _ classes de baixo nível são classes que exercutam tarefas (os detalhes).
+    _ classes de alto nível são classes que gerenciam as classes de baixo
+    _ nível
+*/
---
+++ secao24-SOLID/src/aula337-DIP/main.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ShoppingCart } from "./classes/shopping-cart";
+import { Order } from "./classes/order";
+import { Messaging } from "./services/messaging";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/product";
+import { FiftyPercentDiscount, NoDiscount, TenPercentDiscount } from "./classes/interfaces/discount";
+import { EnterpriseCustomer, IndividualCustomer } from "./classes/customer";
+import { MessagingProtocol } from "./classes/interfaces/messaging-protocol";
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const individualCustomer = new IndividualCustomer(
+    "Luiz",
+    "Miranda",
+    "111.111.111-11",
+);
+const enterpriseCustomer = new EnterpriseCustomer(
+    "Empresa Gigante",
+    "2222222222222222",
+);
+
+class MessagingMock implements MessagingProtocol {
+    sendMessage(): void {
+        console.log("Mensagem enviada pelo mock");
+
+    }
+}
+
+const messagingMock = new MessagingMock();
+
+const order = new Order(
+    shoppingCart,
+    messaging,
+    // messagingMock,
+    persistency,
+    enterpriseCustomer);
+
+
+shoppingCart.addItem(new Product("Camisa", 49.9));
+shoppingCart.addItem(new Product("Caderno", 9.9));
+shoppingCart.addItem(new Product("Lápis", 1.59));
+// shoppingCart.clear();
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
