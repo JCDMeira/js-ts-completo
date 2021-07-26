e0e9768031fac3f92e75848c4d22fd2eb652dcb2
JCDMeira <jean.meira10@hotmail.com>
2021-07-26 00:03:29 -0300

    Adiciona aula 338 - encerramento

---
+++ secao24-SOLID/src/aula338-fechamento/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 338 - Vantagens, desvantagens e DRY, YAGNI, KISS, SOC, MVP
+
+/*
+    _Nota_
+    _ Vantagens e desvantagens S.O.L.I.D
+
+    _Vantagens
+      - código modular
+      - código reutilizável (D.R.Y -> don't repeat yourself)
+      - código testável, baixo acoplamento.
+      - Baixo acoplamento e alta coesão
+      - código expansível
+      - SOC -> separations of concerns (separação de conceitos)
+      - fácil manutensão
+
+    _ desvantagens
+      - maior complexidade
+      - quantidade de código digitado aumenta
+      - tempo de desenvolvimento aumenta bastante
+      - tenha cuidades com:
+        - YAGNI   ->  you aren't gonna need it
+        - KISS    ->  Keep it simple, stupid!
+*/
