ad0a2ef4a0928ca32f40e73fc62fb38202750e1d
JCDMeira <jean.meira10@hotmail.com>
2021-07-27 22:49:58 -0300

    Adiciona aula 342 - spyOn

--- secao25-Jest/src/services/persistency.spec.ts
+++ secao25-Jest/src/services/persistency.spec.ts
@@ -1,4 +1,5 @@
-describe("testando alguma coisa", () => {
+/*
+escribe("testando alguma coisa", () => {
     it("should return one", ()=>{
         const number =1;
         expect(number).toBe(1);
@@ -20,4 +21,31 @@ it("descrição do teste (IT)", ()=>{
 test("descrição do teste (TESTE)", ()=>{
     const nome ="jean";
     expect(nome).not.toBe("não jean");
+});
+*/
+
+import { Persistency } from "./persistency";
+
+describe("Persistency", () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it("Should return undefined", () => {
+        //! System under test
+        const sut = new Persistency();
+        expect(sut.saveOrder()).toBeUndefined();
+    });
+
+    it("Should call console.log once", () => {
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console, "log");
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should call console.log with 'Pedido salvo com sucesso'", () => {
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console, "log");
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledWith("Pedido salvo com sucesso");
+    });
 });
