10fbee1c37c5033a60b96a1978dd73fbe5208a37
JCDMeira <jean.meira10@hotmail.com>
2021-07-28 22:37:52 -0300

    Adiciona aula 350 - coverage report

---
+++ secao25-Jest/src/aulasJest/aula350-testCoverage.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//  * Aula 350 - Test coverage (cobertura de testes)
+
+/*
+    _Nota_
+    _ testa o quanto se cobriu das classes
+    _ gera um relatório
+
+    ! npx jest --coverage
+*/
